5bc55eebe7ec0f7aaaffb45b0faa8bb101e82c8b
Ziyad Helali <ziyad.helali@mail.utoronto.ca>
2018-01-22 22:39:07 -0500

    Queues

---
+++ Queues- A Tale of Two Stacks.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static class MyQueue<T> {
+        Stack<T> stackNewestOnTop = new Stack<T>();
+        Stack<T> stackOldestOnTop = new Stack<T>();
+
+        public void enqueue(T value) { // Push onto newest stack
+           stackNewestOnTop.push(value);
+        }
+
+        public T peek() {
+           if (stackNewestOnTop.empty() && stackOldestOnTop.empty()){
+              return null;
+           }
+           if (!stackOldestOnTop.empty()){
+               return stackOldestOnTop.peek();
+           }
+           while(!stackNewestOnTop.empty()){
+               stackOldestOnTop.push(stackNewestOnTop.pop());
+           }
+           return stackOldestOnTop.peek();
+        }
+
+        public T dequeue() {
+           if (stackNewestOnTop.empty() && stackOldestOnTop.empty()){
+              return null;
+           }
+           if (!stackOldestOnTop.empty()){
+               return stackOldestOnTop.pop();
+           }
+           while(!stackNewestOnTop.empty()){
+               stackOldestOnTop.push(stackNewestOnTop.pop());
+           }
+           return stackOldestOnTop.pop();
+        }
+    }
+
+    
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<Integer>();
+        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+                queue.enqueue(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+                queue.dequeue();
+            } else if (operation == 3) { // print/peek
+                System.out.println(queue.peek());
+            }
+        }
+        scan.close();
+    }
+}
